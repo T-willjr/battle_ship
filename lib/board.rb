4e1adef2f2e5b4c5f3827c566ef56c5c529f4740
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-08 21:31:16 -0500

    Test: Board #exists Test: coordinates have cells

---
+++ lib/board.rb
@@ -0,0 +1,22 @@
+require 'cell'
+
+class Board
+  attr_reader :cell_hash
+
+  def initialize
+    @cell = Cell.new("A1")
+    @cell_hash = {}
+    cells
+  end
+
+  def cells
+    coordinates = ["A1","A2","A3","A4","B1" ,"B2","B3","B4" ,"C1","C2","C3",
+    "C4","D1","D2","D3","D4"]
+
+    coordinates.each do |coordinate|
+      @cell_hash[coordinate] = Cell.new(coordinate)
+    end
+    @cell_hash
+  end
+
+end
