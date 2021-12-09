5a159d637ed2d610d2b01883748ba1f76bd9e09b
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-09 15:59:28 -0500

    feat: Complete The Cells & Validating Placements

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,17 +2,32 @@ require 'pry'
 require './lib/board'
 
 RSpec.describe Board do
-  it "exists" do
-    board = Board.new
-    expect(board).to be_an_instance_of Board
+  context "The Cells" do
+    it "exists" do
+      board = Board.new
+      expect(board).to be_an_instance_of Board
+    end
+
+    it "has cells" do
+      board = Board.new
+
+      expect(board.cell_hash.count).to eq(16)
+
+      board.cell_hash.each do |coordinate, cell|
+        expect(cell).to be_an_instance_of Cell
+      end
+    end
   end
 
-  it "has cells" do
-    board = Board.new
+  context "Validating Coordinates" do
+    it "tells us if a coordinate is on board" do
+      board = Board.new
 
-    board.cell_hash.each do |coordinate, cell|
-      expect(cell).to be_an_instance_of Cell
+      expect(board.valid_coordinate?("A1")).to be true
+      expect(board.valid_coordinate?("D4")).to be true
+      expect(board.valid_coordinate?("A5")).to be false
+      expect(board.valid_coordinate?("E1")).to be false
+      expect(board.valid_coordinate?("A22")).to be false
     end
-    expect(board.cell_hash.count).to eq(16)
   end
 end
