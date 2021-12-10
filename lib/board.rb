224c8044b2eada3007ca11ce379c92f06c61aba2
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-10 13:41:49 -0500

    Feat: Complete #valid_placements?

--- lib/board.rb
+++ lib/board.rb
@@ -25,15 +25,25 @@ class Board
   def valid_placement?(ship, coordinates)
 
     letters = coordinates.map { |coordinate| coordinate.scan(/\D+/).first }
-    a_ord = letters.each_cons(2).all? { |a, b| b.ord == a.ord + 1 }
+    let_ord = letters.each_cons(2).all? { |a, b| b.ord == a.ord + 1 }
 
     nums = coordinates.map { |coordinate| coordinate.scan(/\d+/).first.to_i }
     num_ord = nums.each_cons(2).all? { |a, b| b == a + 1 }
 
-    return false if a_ord == true && num_ord == true
-    true
+    if let_ord == true && num_ord == true || ship.length != coordinates.length || num_ord == false && let_ord == false
+      false
+    else
+      true
+    end
 
-    #ship.length == coordinates.length ***Test 1***
   end
 
 end
+
+# If let_ord is true it goes from A -> B -> C
+# If num_ord is true it goes from 1 -> 2 -> 3
+
+# If let_ord is false letters not alphabetical
+# If num_ord is false numbers not in order
+# ["A1", "B2", "C3"] = false then let_ord && num_ord is true
+# ["A1", "A2", "A4"] = false then num_ord is false
