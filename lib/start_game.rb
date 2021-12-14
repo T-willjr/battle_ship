3edfd77a1a787f0fac7e761858087212e571329b
Maximilian Wagner <maw3350@gmail.com>
2021-12-14 15:56:30 -0500

    feat:add main_menu_options method

--- lib/start_game.rb
+++ lib/start_game.rb
@@ -1,11 +1,24 @@
 require './lib/board'
 
 class StartGame
+  def initialize
+      computer = ComputerPlayer.new
+  end
 
   def main_menu
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit."
     main_menu_input = gets.chomp.upcase
     main_menu_options(main_menu_input)
+  end
+
+  def main_menu_options(user_input)
+    case user_input
+    when "P"
+      "test"
+      computer.computer_setup
+    when "Q"
+      exit
+    end
   end
 end
