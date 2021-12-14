dd0a216c14cea2ab9ee7a9ffe3a6829ecd2cfce8
Maximilian Wagner <maw3350@gmail.com>
2021-12-13 20:27:18 -0500

    fix:place method now works (praise Jesus)

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,3 @@
-# require 'cell'
 require 'pry'
 
 class Board
@@ -6,16 +5,20 @@ class Board
 
   def initialize
     @cell_hash = {}
-    cells
+    make_cells
   end
 
   def cells
+    @cell_hash
+  end
+
+  def make_cells
     coordinates = ["A1","A2","A3","A4","B1" ,"B2","B3","B4" ,"C1","C2","C3","C4","D1","D2","D3","D4"]
 
     coordinates.each do |coordinate|
       @cell_hash[coordinate] = Cell.new(coordinate)
     end
-    @cell_hash
+
   end
 
   def valid_coordinate?(coordinate)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -73,11 +73,11 @@ RSpec.describe Board do
       cell_4 = board.cells["A4"]
 
       board.place(cruiser, ["A1", "A2", "A3"])
-# binding.pry
+
       expect(cell_1.ship).to eq(cruiser)
-      # expect(cell_2.ship).to eq(cruiser)
-      # expect(cell_3.ship).to eq(cruiser)
-      # expect(cell_4.ship).to eq(nil)
+      expect(cell_2.ship).to eq(cruiser)
+      expect(cell_3.ship).to eq(cruiser)
+      expect(cell_4.ship).to eq(nil)
 
     end
 
