e91ec80f83e128a40b6da35c6413770d8331be51
Maximilian Wagner <maw3350@gmail.com>
2021-12-14 16:46:45 -0500

    feat:add cruiser_placement

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,29 @@
 require "./lib/board"
+require "./lib/ship"
 class Player
 
   def initialize
     @player_board = Board.new
   end
 
-  def player_setup
+  def game_explaination
     puts "I have laid out my ships on the grid.
     You now need to lay out your two ships.
     The Cruiser is three units long and the Submarine is two units long."
-    puts @player_board.render
-    cruiser_placement
   end
 
-  def cruiser_placement
+  def player_setup
+    game_explaination
+    puts @player_board.render
     print "Enter the squares for the Cruiser (3 spaces):\n>"
-    cruiser_imput = gets.chomp
+    cruiser_input = gets.chomp.split
+    cruiser_placement(cruiser_input)
+  end
+
+  def cruiser_placement(cruiser_input)
+    cruiser = Ship.new("Cruiser", 3)
+    @player_board.place(cruiser, cruiser_input)
+    puts @player_board.render(true)
   end
 end
 
